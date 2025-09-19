a47b901d88fc33ae3251812585c30b21f59efc9d
Saravanakumar Sathasivam <saravanakumars@xerago.com>
2025-09-19 11:46:19 +0530

    commit after removing server folder

--- server/src/routes/admin.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-export const router = Router();
-
-router.get("/stats", async (_req, res) => {
-	res.json({ users: 0, threads: 0, posts: 0, events: 0 });
-});
-
-
--- server/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-export * as auth from "./auth";
-export * as users from "./users";
-export * as forums from "./forums";
-export * as events from "./events";
-export * as knowledge from "./knowledge";
-export * as leaderboard from "./leaderboard";
-export * as admin from "./admin";
-
-
